158cd8a52e18df584822635468139d1c3d46eb5e
sreyeg <santiagoreyes.g@gmail.com>
2025-01-27 20:56:43 -0800

    Configure tailwind

--- scorecollector/settings.py
+++ scorecollector/settings.py
@@ -40,6 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
+]
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 MIDDLEWARE = [
