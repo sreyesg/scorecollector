0125723734c03ae0dd8a4527de7a0535ac723da8
sreyeg <santiagoreyes.g@gmail.com>
2025-01-27 19:31:56 -0800

    Last Commit

--- scorecollector/settings.py
+++ scorecollector/settings.py
@@ -124,5 +124,4 @@ LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'home'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
